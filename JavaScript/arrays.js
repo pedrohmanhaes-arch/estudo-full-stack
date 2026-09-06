d1a4b2978b676bff5e92659d42fbb410e003cba8
Pedro Henrique Moreci Manhães de Oliveira <pedrohmanhaes@gmail.com>
2026-09-06 20:32:04 -0300

    Aula 157: JS Arrays

---
+++ JavaScript/arrays.js
@@ -0,0 +1,12 @@
+let nome = "Jamilton"
+let nome1 = "Leticia"
+
+let nomes = ["Jamilton", "Letícia", "Maria"]
+
+//nomes.push("Novo item")
+
+// nomes[0] = "Teste" 
+
+// delete nomes[0]
+
+console.log(nomes[0]);
