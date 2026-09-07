3c1c5f5d2371054e9908e6017e7ca97a9dae9b81
Pedro Henrique Moreci Manhães de Oliveira <pedrohmanhaes@gmail.com>
2026-09-07 19:40:08 -0300

    aula 162: Projeto Lista Nomes

---
+++ JavaScript/listas-nomes/js/script.js
@@ -0,0 +1,38 @@
+let nomes= ["jamilton", "josé", "letícia", "maria", "carlos", "pedro", "marcela", "claudia", "carla", "carlos"]
+
+function pesquisarNome(){
+
+    let nomePesquisa = document.getElementById('campoNome').value
+     let itensLista = '' 
+       
+    for(indice in nomes){
+        let nome = nomes[indice]
+        if( nomePesquisa == nome) {
+            itensLista +=`
+            <li class="list-group-item">
+            ${nome}
+            </li>`
+        }
+    } 
+       document.getElementById('lista').innerHTML = itensLista 
+}
+
+
+
+function carregarNomes(){
+
+    let itensLista = ''
+    for(indice in nomes){
+
+        let nome = nomes[indice]
+        
+        itensLista += `
+        <li class="list-group-item">
+            ${nome}
+        </li>`
+        
+        //alert(itensLista)
+    }
+
+    document.getElementById('lista').innerHTML = itensLista
+}
