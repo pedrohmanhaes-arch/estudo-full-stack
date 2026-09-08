9554f5370a7ef9daf3ec9eb1f8c4aff2c908d91f
Pedro Henrique Moreci Manhães de Oliveira <pedrohmanhaes@gmail.com>
2026-09-08 17:28:58 -0300

    Aula 173: encapsulamentopt1

---
+++ JavaScript/encapsulamento.js
@@ -0,0 +1,30 @@
+/* // Pilar - Encapsulamento
+class Carro{
+  constructor(){
+    this.modelo = "Gol"
+    this.cor = "Vermelho"  
+  }
+  frear(){
+  /* freio com tecnologia a tambor
+  .
+  .
+  .
+  
+   freio com tecnologia a disco
+  .
+  .
+  .
+  .
+  .
+  .
+  .
+  .
+  .
+  .
+  
+    console.log("parar o carro")
+  }
+}
+
+const carro = new Carro()
+carro.frear() */
