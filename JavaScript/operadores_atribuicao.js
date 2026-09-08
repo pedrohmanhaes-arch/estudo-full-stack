0119506964d4c151f17af55812cfaf3af4474354
Pedro Henrique Moreci Manhães de Oliveira <pedrohmanhaes@gmail.com>
2026-09-07 21:04:55 -0300

    aula 163: operadores_atribuicao

---
+++ JavaScript/operadores_atribuicao.js
@@ -0,0 +1,20 @@
+let salario =500
+let bonus = 200
+
+salario = salario + bonus  // = 700
+
+salario = bonus = // 200
+salario += bonus // = 700
+
+let desconto = 100
+salario = salario - desconto // = 400
+salario -= desconto // = 400
+
+let mult = 2
+salario = salario * mult // = 1000
+salario *= mult // = 1000
+
+let div = 2
+
+salario = salario / div // = 250
+salario /= div // = 250
