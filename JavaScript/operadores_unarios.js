bf72e111fd548d767e21ea4b135d3b584f311c18
Pedro Henrique Moreci Manhães de Oliveira <pedrohmanhaes@gmail.com>
2026-09-07 21:17:13 -0300

    aula 164: JS operadores unarios

---
+++ JavaScript/operadores_unarios.js
@@ -0,0 +1,13 @@
+// Operadores unários
+let numero = 1
+
+
+// Pré fixado
+//console.log(++numero)
+//++numero + 2 // = 4
+--numero + 2 // = 1 - 1 + 2 = 2
+
+// Pós fixado 
+//console.log(numero++)
+ console.log(numero-- + 2) //1 + 2 = 3
+ console.log(numero) //1 - 1  = 0
