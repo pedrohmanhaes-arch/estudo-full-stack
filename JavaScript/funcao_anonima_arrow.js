07deac00b8040cc46f464987354a8ba7816c7283
Pedro Henrique Moreci Manhães de Oliveira <pedrohmanhaes@gmail.com>
2026-09-07 21:50:08 -0300

    aula 166: JS fucções anonimas, arrow

---
+++ JavaScript/funcao_anonima_arrow.js
@@ -0,0 +1,35 @@
+// + Função literal
+
+// function somar(a,b){
+//     console.log(a+b)
+
+// }
+// somar(1,2)
+
+//+ Função anônima (sem nome)
+
+/* const somar = function(a,b){
+    console.log(a+b)    
+} 
+
+somar(5,13)*/
+
+//+ Função arrow ( seta )
+
+/* const somar = (a,b) => {
+    console.log(a+b)
+}
+
+somar(52,98) */
+
+
+//+ Função arrow ( retorno implícito)
+
+    //const somar = (a,b) => console.log(a+b)
+   // const exibir = n => console.log("nome: " + n)
+    //const somar = (a,b) => a+b 
+    
+    //exibir("Jamilton")
+let resultado = somar(1,2)
+//somar(52,98) 
+
