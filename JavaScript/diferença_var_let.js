444831810d3699afd43a424c565f0ec166e8b2ec
Pedro Henrique Moreci Manhães de Oliveira <pedrohmanhaes@gmail.com>
2026-09-07 21:37:51 -0300

    aula 165: Diferença Var e Let

---
+++ JavaScript/diferença_var_let.js
@@ -0,0 +1,20 @@
+// Var vs let  
+// var =  escopo global, função 
+// let = escopo global, função, local (bloco)
+
+// var numero = 10 //global
+
+
+// let numero = 5    
+// console.log(numero) 
+
+// }
+
+function calcular(){
+    var numero = 12
+}
+
+// calcular()
+// numero = 20
+
+//console.log(numero)
