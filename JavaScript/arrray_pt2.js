7a32b9ffbf88d1e081aeb103eae5d4774edd0e9e
Pedro Henrique Moreci Manhães de Oliveira <pedrohmanhaes@gmail.com>
2026-09-10 10:41:13 -0300

    Aula 191: Js arrray_pt2

---
+++ JavaScript/arrray_pt2.js
@@ -0,0 +1,31 @@
+/*const lista = []
+
+const lista2 = new Array("item")
+
+typeof[]
+
+const nomes = ["Jamilton", "Maria"]
+const frutas = new Array("Morango", "Banana")
+
+console.log(nomes)
+console.log(frutas[1])*/
+
+const nomes = ["Jamilton", "Maria", "Ana"]
+
+nomes.push("Pedro") // adicionar novo item
+
+console.log(nomes)
+
+// delete nomes[2] // deletar posição
+// console.log(nomes)
+
+// nomes[0] = "Julio"
+
+console.log(nomes)
+
+nomes.push("João")
+
+nomes.sort() // classifiacr em ordem
+
+console.log(nomes)
+
