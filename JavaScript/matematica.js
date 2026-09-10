ed88a9f2b9281396ef2c69b20a781560f82ee2e7
Pedro Henrique Moreci Manhães de Oliveira <pedrohmanhaes@gmail.com>
2026-09-09 23:45:54 -0300

    aula 189: JS matematica

---
+++ JavaScript/matematica.js
@@ -0,0 +1,13 @@
+let numero = 280.40
+
+Math.PI
+Math.ceil(numero) //arredonda pra cima
+Math.floor(numero) // arredonda pra baixo
+Math.round(numero) //arredonda pra cima ou pra baixo
+Math.random() // gera numero decimal aleatorio entre 0 e 1
+Math.random() * 100 // gera numero decimal aleatorio entre 0 e 100
+Math.min(15,10, 100, 1) //gera o menor número entre os indicado
+Math.max(15,10, 100, 1) //gera o maior número entre os indicado
+Math.pow(2,10) // um numero elevado ao outro
+Math.sqrt(9) // raiz quadrada
+
