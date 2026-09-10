4b8e995c2a32b9a0ed26ed5618b6f191fc15619c
Pedro Henrique Moreci Manhães de Oliveira <pedrohmanhaes@gmail.com>
2026-09-09 23:38:40 -0300

    aula 188: JS manipulacao_textos

---
+++ JavaScript/manipulacao_textos.js
@@ -0,0 +1,53 @@
+//Funções Nativas - Manipulação de textos
+
+/*const nome = {
+  texto: "já",
+  length: 2,
+  charAt(){
+    
+  }
+}*/
+
+//"jamilton".length
+
+
+// nome.length
+// nome.charAt()
+// let n = nome.replace("jamilton", "pedro")
+// let nome = "carro do jamilton"
+
+// console.log(n)
+
+// let frase = "O sucesso é ir de fracasso em fracasso sem perder o entusiasmo"
+
+// frase.substr(0, 15) + " ..."
+
+/*let nome = "jamilton damasceno silva"
+
+let resultado = nome.split(" ")
+console.log(resultado)
+
+let nome = "jamilton"
+let sobrenome = "damasceno"
+let s = sobrenome.slice(0,4)
+
+nome + s + "@empresa.com.br"
+
+let pesquisa = "PEdRo sIlVa"
+let itemSalvo = "pedro silva"
+itemSalvo.toUpperCase()
+pesquisa.toLowerCase()
+
+let item = pesquisa.toLowerCase()
+if(itemSalvo = item){
+  console.log("Achei")
+
+let nome = "   Pedro Silva    "
+nome.trim()*/
+
+
+let a = "Olá"
+let b = "Jamilton"
+let c = "Damasceno"
+
+a.concat(" ",b," ",c)
