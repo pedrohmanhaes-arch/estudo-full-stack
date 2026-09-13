620900544787fdc68cd9b8aad38b93cdf828e874
Pedro Henrique Moreci Manhães de Oliveira <pedrohmanhaes@gmail.com>
2026-09-13 20:00:11 -0300

    ex009 condição composta

---
+++ JavaScript/curso.em.video/aula11/ex009.js
@@ -0,0 +1,9 @@
+var pais = 'Brasil'
+console.log(`Vivendo em ${pais}`)
+
+//condição composta
+if(pais == 'Brasil'){
+    console.log("Você é Brasileiro")
+}else{
+    console.log("Você é estrangeiro")
+}
