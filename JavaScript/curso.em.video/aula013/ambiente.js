6a6f78d44e31c3e5dc44dd7087d9edc6178d7590
Pedro Henrique Moreci Manhães de Oliveira <pedrohmanhaes@gmail.com>
2026-09-17 23:14:55 -0300

    aula 13 repetições

---
+++ JavaScript/curso.em.video/aula013/ambiente.js
@@ -0,0 +1,30 @@
+var c = 1
+do{
+    console.log(`passo ${c}`)
+    c++
+}while (c<=6)
+
+/*var c = 1
+while(c<=10){
+    console.log(`Passo ${c}`)
+    c++
+}
+
+
+
+var c = 1
+while(c<=6){
+    console.log("tudo bem")
+    c++
+}
+
+
+
+
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')*/
