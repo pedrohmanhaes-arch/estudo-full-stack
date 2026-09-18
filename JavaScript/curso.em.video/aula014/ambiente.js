35658fe17123d71d3fa4ba75899eaa2d64496bfd
Pedro Henrique Moreci Manhães de Oliveira <pedrohmanhaes@gmail.com>
2026-09-18 12:36:07 -0300

    Aula 14 for

---
+++ JavaScript/curso.em.video/aula014/ambiente.js
@@ -0,0 +1,17 @@
+/* var c = 1
+while(c<=5){
+    console.log(c)
+    c += 1
+} */
+
+    /* for(c = 1;c <= 25; c++){
+        console.log(`${c} carneirinho`)
+    } */
+
+    console.log("Vai começar...")
+    for(var c = 1 ; c <= 4; c++){
+        console.log(c)
+    }
+    console.log('FIM')
+
+    
