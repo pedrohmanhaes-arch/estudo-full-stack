ad1d897415a8c3d8c2949b84041966fd5290d6f8
Pedro Henrique Moreci Manhães de Oliveira <pedrohmanhaes@gmail.com>
2026-09-20 14:48:13 -0300

    Aula 16 funcao 2

---
+++ JavaScript/curso.em.video/aula015/aula016/funcao02.js
@@ -0,0 +1,19 @@
+/* function soma(n1 = 0, n2 = 0){
+    return n1 + n2
+}
+
+console.log(soma(4,9)) */
+
+/* function soma(n1 = 0, n2 = 0){
+    console.log(n1 + n2)
+}
+
+soma(57,69)*/
+
+/* function soma(n1, n2) {return `a soma dos número ${n1} e ${n2} é ${n1+n2}`}
+
+console.log(soma(22, 63)) */
+
+const soma = (n1 = 0, n2 = 0) => `a soma dos número ${n1} e ${n2} é: ${n1+n2}`
+
+console.log(soma(56,75))
